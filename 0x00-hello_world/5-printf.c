67dd505392a6972a599bf441d96972a9e54850c1
JoeNagalila <pwabwotika@gmail.com>
2023-06-15 01:06:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A C Programme that prints a line using  a printif function
+ * Return: 0 (Passed)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
