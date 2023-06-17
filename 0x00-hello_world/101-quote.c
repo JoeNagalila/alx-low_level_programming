5eb3292144be86d4ee3c5fc4399e2a4a62d7def1
JoeNagalila <pwabwotika@gmail.com>
2023-06-17 15:29:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -  A c Program that prints a line to the standard error
- * Return: 1(Success)
+ * main - prints the last part of the quote in the standard error
+ * Retrun: 1 (Success)
  */
 int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar ,2015-10-19\n", 59);
 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
